30d74b5bf9929befbfb694826911746a0074264a
Raziel <raziela@gmc.gov.il>
2022-05-22 07:19:04 +0300

    deploy- 22.5.22

--- src/server/initialize-db.js
+++ src/server/initialize-db.js
@@ -4,9 +4,11 @@ import { connectDB } from "./connect-db";
 async function initializeDB(){
     let db = await connectDB();
     let user = await db.collection(`users`).findOne({id:"U1"});
+    console.log("User?".user);
     if(!user){
 
         for(let collectionName in defaultState){
+            console.log("initializing collection",collectionName);
             let collection = db.collection(collectionName);
             await collection.insertMany(defaultState[collectionName]);
         }
--- src/server/server.js
+++ src/server/server.js
@@ -4,8 +4,11 @@ import bodyParser from 'body-parser';
 import { connectDB } from './connect-db';
 import './initialize-db';
 import { authenticationRoute } from './authenticate';
+import path from 'path';
 
-let port = 7777;
+
+let port = process.env.PORT || 7777;
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 let app = express();
 
 app.listen(port, console.log("Server listening on port", port));
@@ -21,6 +24,13 @@ app.use(
 );
 
 authenticationRoute(app);
+
+if(process.env.NODE_ENV == `production`){
+    app.use(express.static(path.resolve(__dirname,`../../dist`)));
+    app.get('/*',(req,res) =>{
+        res.sendFile(path.resolve('index.html'));
+    });
+}
 
 export const addNewTask = async task =>{
     let db = await connectDB();
