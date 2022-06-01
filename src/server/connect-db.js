83820b0612ae5c1c2fd5e801c4577c47eb91f7dc
Raziel <razavraham2@gmail.com>
2022-06-01 14:55:27 +0300

    deploy-1.6.22-5

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -3,6 +3,8 @@ const url = process.env.MONGODB_URI || `mongodb://localhost:27017/myorganizer`;
 let db = null;
 const DB_NAME = "myAppReactDB";
 const DB_COLLECTION_NAME = "users";
+
+
 export async function connectDB(){
     if(db){
         return db;
@@ -11,7 +13,7 @@ export async function connectDB(){
     .then(() => console.log("Connection Successful"))
     .catch(err => console.log(err));;
     try {
-    db = client.db(DB_NAME);
+    db = await client.db(DB_NAME);
     const collection = db.collection(DB_COLLECTION_NAME);
     const res = await collection.find().toArray();
     console.log(res);
