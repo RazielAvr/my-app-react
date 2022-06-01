6fd3adeb20f49e2f0c66c7df04f964d5ab2d93ff
Raziel <razavraham2@gmail.com>
2022-06-01 12:39:03 +0300

    deploy-1.6.22-3

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb';
 const url = process.env.MONGODB_URI || `mongodb://localhost:27017/myorganizer`;
 let db = null;
+const DB_NAME = "myAppReactDB";
+const DB_COLLECTION_NAME = "users";
 export async function connectDB(){
     if(db){
         return db;
@@ -8,7 +10,20 @@ export async function connectDB(){
     let client = await MongoClient.connect(url, {useNewUrlParser: true})
     .then(() => console.log("Connection Successful"))
     .catch(err => console.log(err));;
-    db = client.db();
+    try {
+    db = client.db(DB_NAME);
+    const collection = db.collection(DB_COLLECTION_NAME);
+    const res = await collection.find().toArray();
+    console.log(res);
+    return res
+    }
+ catch (err) {
+    console.log(err);
+    return err;    
+}
+finally {
+    client.close();
+}
     console.info("Got DB", db);
     return db;
 }
