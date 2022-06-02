ef1806a8ae93a19ae8409628d115a7a244e34aef
Raziel <razavraham2@gmail.com>
2022-06-02 16:11:54 +0300

    deploy-2.6.22-7

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -24,6 +24,7 @@ const mapDispatchToProps = (dispatch) =>({
 
     authenticateUser(e){
         e.preventDefault();
+        console.log(e.target[`username`].value);
         let username = e.target[`username`].value;
         let password = e.target[`password`].value;
         dispatch(mutations.requestAuthenticateUser(username,password));
--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -4,8 +4,7 @@
 import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI;
 const options = {
-   useUnifiedTopology: true,
-   useNewUrlParser: true,
+   useNewUrlParser: true
 };
 let client;
 let clientPromise;
