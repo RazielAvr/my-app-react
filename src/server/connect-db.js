e6c5ecf65d72dd521e1a93e9bedb7cf91fe6ec78
Raziel <razavraham2@gmail.com>
2022-06-29 11:29:29 +0300

    deploy-29.6.22-6

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -14,13 +14,13 @@ export async function connectDB(){
     let clientOut = await MongoClient.connect(url, {useNewUrlParser: true}).then(client => {
         console.log('Connected to MongoDB server')
     
-        let _db = client.db(DB_NAME);
+        const _db = client.db(DB_NAME);
         _db.listCollections().toArray(function(err, names) {   
        if(!err) {
            //console.log(names)
            console.info("Got DB");
        }
-    //    return _db;
+        return _db;
    });
 }).catch((err) => {
  
@@ -28,24 +28,6 @@ export async function connectDB(){
    console.log(err.Message);
 }, e => console.log('Error to connect', e))
 
-
-//     .then(() => console.log("Connection Successful"))
-//     .catch(err => console.log(err));
-//     try {
-//     db = client.db(DB_NAME);
-//     const collection = db.collection(DB_COLLECTION_NAME);
-//     const res = await collection.find().toArray();
-//     console.log(res);
-//     return res
-//     }
-//  catch (err) {
-//     console.log(err);
-//     return err;    
-// }
-// finally {
-//     console.info("Got DB", db);
-//     return db;
-// }
 db = clientOut.db();
 return db;
 }
