69a414965fcce3db8a93ed53b64183ca13481408
Raziel <razavraham2@gmail.com>
2022-06-29 11:42:25 +0300

    deploy-29.6.22-9

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -5,28 +5,33 @@ const url = process.env.MONGODB_URI || `mongodb://localhost:27017/myorganizer`;
 const db = null;
 const DB_NAME = 'myAppReactDB';
 const DB_COLLECTION_NAME = 'users';
+let client;
 
 
 export async function connectDB(){
-    if(db){
-        return db;
-    }
-    let clientOut = await MongoClient.connect(url, {useNewUrlParser: true}).then(client => {
-        console.log('Connected to MongoDB server')
+    if (db) return db;
+    let client = await MongoClient.connect(url, { useNewUrlParser: true });
+    db = client.db();
+    return db;
+//     if(db){
+//         return db;
+//     }
+//     MongoClient.connect(url, {useNewUrlParser: true}).then(client => {
+//         console.log('Connected to MongoDB server')
     
-         db = client.db(DB_NAME);
-        _db.listCollections().toArray(function(err, names) {   
-       if(!err) {
-           //console.log(names)
-           console.info("Got DB");
-       }
-        return db;
-   });
-}).catch((err) => {
+//          db = client.db(DB_NAME);
+//         _db.listCollections().toArray(function(err, names) {   
+//        if(!err) {
+//            //console.log(names)
+//            console.info("Got DB");
+//        }
+//         return db;
+//    });
+// }).catch((err) => {
  
-   // Printing the error message
-   console.log(err.Message);
-}, e => console.log('Error to connect', e))
+//    // Printing the error message
+//    console.log(err.Message);
+// }, e => console.log('Error to connect', e))
 
 }
 
